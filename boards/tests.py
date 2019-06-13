705fd6d5e4703793469f97dc97817e7c3da4974d
dabutvin <butvinik@outlook.com>
2019-06-13 12:18:11 -0700

    create topics view for a board

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
-from .views import home
+
+from .models import Board
+from .views import home, board_topics
 
 
 class HomeTests(TestCase):
@@ -11,3 +13,20 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        response = self.client.get('/boards/1/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_notfound_status_code(self):
+        response = self.client.get('/boards/12124/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_board_topics_url_resolves_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEqual(view.func, board_topics)
