66b78f4140536ca5c662e070aee852762be1e27b
dabutvin <butvinik@outlook.com>
2019-06-13 11:42:23 -0700

    render html template

--- boards/views.py
+++ boards/views.py
@@ -5,11 +5,5 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    board_names = list()
 
-    for board in boards:
-        board_names.append(board.name)
-
-    response_html = '<br>'.join(board_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
