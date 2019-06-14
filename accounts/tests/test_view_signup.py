0201ab51a39b5c60e21a3199b90024230bd4c935
dabutvin <butvinik@outlook.com>
2019-06-14 13:43:15 -0700

    break tests into multiple files

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve
 
-from .forms import SignUpForm
-from .views import signup
+from ..forms import SignUpForm
+from ..views import signup
 
 
 class SignUpTests(TestCase):
